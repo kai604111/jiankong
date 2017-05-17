f0976ff3781456717d54c8d9e321593a3d4be1cf
wzk <604111884@qq.com>
2017-05-17 11:30:45 +0800

    first commit

---
+++ mail.js
@@ -0,0 +1,203 @@
+﻿var http = require('http');
+var nodemailer = require('nodemailer'); //nodemailer模块，用于发送邮件
+var checkServerJSONfile = "http://116.210.254.248/moniter.json"; //请求服务器状态文件，用于服务器是否挂掉
+var checkWebStaticfile1 = "http://116.210.254.227/app/static/js/keyCode.js"; //请求工程的静态文件，用于判断工程是否挂掉
+var checkWebStaticfile2 = "http://116.210.254.227/app/static/js/keyCode.js"; //请求工程的静态文件，用于判断工程是否挂掉
+
+//服务器状态阈值
+var warnFlag = {
+    "disk": 80,
+    "nginx": 0,
+    "mysql_status": 0,
+    "mysql_slave": 2
+};
+// 记录服务器状态参数发生的错误
+var serverErrorFlag = {
+    "hubei_172": {
+        "disk": true,
+        "nginx": true,
+        "mysql_status": true,
+        "mysql_slave": true
+    },
+    "hubei_173": {
+        "disk": true,
+        "nginx": true,
+        "mysql_status": true,
+        "mysql_slave": true
+    }
+};
+// 邮件发送配置
+// var mails = {
+//     form: 'server<joyuservermail@163.com>',
+//     to: ' liwenyang@joyutech.com',
+//     cc: 'yuanfanglin@joyutech.com,yangzheng@joyutech.com,chenhongyu@joyutech.com,lengweiping@joyutech.com,guoziao@joyutech.com'
+// };
+// 邮件发送配置
+var mails = {
+    form: '604111884@qq.com',
+    to: '604111884@qq.com',
+    cc: 'wangzhenkai@joyutech.com,604111884@qq.com'
+};
+//邮件配置：from发送方，to接收方，cc配置邮件抄送对象。subject邮件主题，text邮件的内容。
+var erroroption = {
+    from: mails.form,
+    to: mails.to,
+    cc: mails.cc,
+    subject: "工程出错",
+    text: "服务器挂掉了，请尽快处理。"
+};
+var restartoption = {
+    from: mails.form,
+    to: mails.to,
+    cc: mails.cc,
+    subject: "工程恢复",
+    text: "服务器已恢复。"
+};
+//配置邮件的服务商与协议
+var mail = nodemailer.createTransport({
+    host: "smtp.qq.com",
+    server: 'qq',
+    port: 465,//配置SMTP端口
+    secureConnection: true, //SSL加密通道，防止邮件被截获
+    //auth设置使用SMTP协议的邮箱以及授权码
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
+var restart = true;//判断程序是否已启动
+var isVPN = 0;//由于VPN连接可能会出现断了重连现象，导致误发邮件。所以请求5次都请求不到的话，才判断为工程出错或者VPN断了不连了。
+var mailErrorFlag = false; // 只有服务器参数超出范围时才需要发送邮件；
+var oneHour = true; // 发生错误时，一小时发送一次邮件
+// 根据请求文件内容判断是否发送邮件
+function checkServer() {
+    http.get(checkServerJSONfile, function (res) {
+        console.log('get response Code :' + res.statusCode);
+        if (res.statusCode == 200) {
+            var json = '';
+            res.on('data', function (d) {
+                json += d;
+            });
+            res.on('end', function () {
+                //获取到的数据
+                json = JSON.parse(json);
+                console.log(json);
+                var revover = function (ipDesc,type) {
+                    var _text = restartoption.text;
+                    if (!serverErrorFlag[ipDesc][type]) {
+                        restartoption.text = type + ":状态已恢复";
+                        mail.sendMail(restartoption, function (error, info) {
+                            var date = new Date();
+                            console.log(date + '状态已恢复 ');
+                            serverErrorFlag[ipDesc][type] = true;
+                            oneHour = true;
+                        });
+                        serverErrorFlag[ipDesc][type] = true;
+                    }
+                    restartoption.text = _text;
+                };
+                var sendStr = "";
+                var judeJSON = function (ip, json, erroroption) {
+                    // console.log(json);
+                    var str = {};
+                    str['ip'] = ip;
+                    if (json['disk'] > warnFlag['disk']) {
+                        str['disk'] = "服务器硬盘报警！当前硬盘占用率：" + json['disk'];
+                        serverErrorFlag[ip]['disk'] = false;
+                        mailErrorFlag = true;
+                    } else {
+                        revover(ip,'disk');
+                    }
+                    if (json['nginx'] != warnFlag['nginx']) {
+                        str['nginx'] = "nginx报警！当前nginx状态：" + json['nginx'];
+                        serverErrorFlag[ip]['nginx'] = false;
+                        mailErrorFlag = true;
+                    } else {
+                        revover(ip,'nginx');
+                    }
+                    if (json['mysql_status'] != warnFlag['mysql_status']) {
+                        str['mysql_status'] = "数据库报警！当前数据库状态：" + json['mysql_status'];
+                        serverErrorFlag[ip]['mysql_status'] = false;
+                        mailErrorFlag = true;
+                    } else {
+                        revover(ip,'mysql_status');
+                    }
+                    if (json['mysql_slave'] != warnFlag['mysql_slave']) {
+                        str['mysql_slave'] = "数据库同步报警！当前数据库同步状态：" + json['mysql_slave'];
+                        serverErrorFlag[ip]['mysql_slave'] = false;
+                        mailErrorFlag = true;
+                    } else {
+                        revover(ip,'mysql_slave');
+                    }
+                    sendStr += JSON.stringify(str);
+                };
+                judeJSON("hubei_172", json["hubei_172"], erroroption);
+                judeJSON("hubei_173", json["hubei_173"], erroroption);
+
+                var erroroption_text = erroroption.text;
+                console.log(sendStr);
+                if (sendStr != "" && sendStr.length != 0 && mailErrorFlag && oneHour) {
+                    console.log("sendServerMail");
+                    erroroption.text = sendStr;
+                    mail.sendMail(erroroption, function (error, info) {
+                        var date = new Date();
+                        console.log(date + '服务器状态出现问题');
+                        oneHour = false;
+                    });
+                    erroroption.text = erroroption_text;
+                }
+            });
+        }
+        if (res.statusCode == 404) {
+            var erroroption_text = erroroption.text;
+            erroroption.text = "服务器状态文件访问失败，请检查文件。";
+            mail.sendMail(erroroption, function (error, info) {
+                var date = new Date();
+                console.log(date + '服务器挂掉了');
+            });
+            erroroption.text = erroroption_text;
+        }
+    }).on('error', function (e) {
+        console.log("Got error1 :" + e.message);
+        var erroroption_text = erroroption.text;
+        erroroption.text = "服务器状态文件访问失败，请检查文件。";
+        mail.sendMail(erroroption, function (error, info) {
+            var date = new Date();
+            console.log(date + '服务器挂掉了');
+        });
+        erroroption.text = erroroption_text;
+    });
+}
+// 请求web项目中的静态文件判断项目是不是在服务
+function checkWeb(accessUrl) {
+    http.get(accessUrl, function (res) {
+        //console.log('get response Code :' + res.statusCode);
+        isVPN = 0;
+        if (!restart) {
+            mail.sendMail(restartoption, function (error, info) {
+                var date = new Date();
+                console.log(date + '工程已恢复 ');
+                restart = true;
+            });
+        }
+    }).on('error', function (e) {
+        console.log("Got error2 :"+e.message);
+        if (e.message != 'read ECONNRESET') {
+            isVPN++;
+        }
+        if (restart && e.message != 'read ECONNRESET' && isVPN == 5) {
+            mail.sendMail(erroroption, function (error, info) {
+                var date = new Date();
+                console.log(date + '工程挂掉了');
+                restart = false;
+            });
+        }
+    });
+}
+
+setInterval(function () {
+    console.log("start setInterval");
+    checkServer();
+    checkWeb(checkWebStaticfile1);
+    checkWeb(checkWebStaticfile2);
+}, 5 * 1000);//检测频率 60S一次
---
+++ server.js
@@ -0,0 +1,32 @@
+﻿var http = require('http');
+
+var messages = [
+  'Hello World',
+  'From a basic Node.js server',
+  'Take Luck'];
+http.createServer(function (req, res) {
+  res.setHeader("Content-Type", "application/json;charset=UTF-8");
+  res.writeHead(200);
+  res.write('<html><head><title>Simple HTTP Server</title></head>');
+  res.write('<body>');
+  res.end('');
+}).listen(8080);
+
+var options = {
+    hostname: 'localhost',
+    port: '8080'
+  };
+function handleResponse(response) {
+  var serverData = '';
+  response.on('data', function (chunk) {
+    serverData += chunk;
+  });
+  response.on('end', function () {
+    console.log("Response Status:", response.statusCode);
+    console.log("Response Headers:", response.headers);
+    console.log(serverData);
+  });
+}
+http.request(options, function(response){
+  handleResponse(response);
+}).end();
