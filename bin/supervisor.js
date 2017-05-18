ea60ac540a2469c0cb35c32ea9121626819aa56d
wzk <604111884@qq.com>
2017-05-19 00:08:56 +0800

    生成 package.json 更新监督配置

--- bin/supervisor.js
+++ bin/supervisor.js
@@ -3,42 +3,12 @@
  */
 var http = require('http');
 var nodemailer = require('nodemailer'); //nodemailer模块，用于发送邮件
+var conf = require('../conf/supervisor_conf').config;
 var port = 30000;
 var lastNotifyTime = null;
 
-// 邮件发送配置
-var mails = {
-    form: 'liwenyang@joyutech.com',
-    to: 'wangzhenkai@joyutech.com',
-    cc: 'wangzhenkai@joyutech.com'
-};
-//邮件配置：from发送方，to接收方，cc配置邮件抄送对象。subject邮件主题，text邮件的内容。
-var erroroption = {
-    from: mails.form,
-    to: mails.to,
-    cc: mails.cc,
-    subject: "监督服务",
-    text: "湖北监控服务异常，请及时修复！"
-};
-var restartoption = {
-    from: mails.form,
-    to: mails.to,
-    cc: mails.cc,
-    subject: "监督服务",
-    text: "湖北监控服务已恢复。"
-};
 //配置邮件的服务商与协议
-var mail = nodemailer.createTransport({
-    host: "smtp.exmail.qq.com",
-    server: 'qq',
-    port: 465,//配置SMTP端口
-    secureConnection: true, //SSL加密通道，防止邮件被截获
-    //auth设置使用SMTP协议的邮箱以及授权码
-    auth: {
-        user: 'liwenyang@joyutech.com',
-        pass: 'Joyu1201'
-    }
-});
+var mail = nodemailer.createTransport(conf.createMailerConf);
 var server = http.createServer(function (req, res) {
     console.log('URL: ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -46,7 +16,7 @@ var server = http.createServer(function (req, res) {
     lastNotifyTime = new Date().getTime();
     console.log("lastNotifyTime : " + lastNotifyTime);
     if (!restart && isRecoverSend) {
-        mail.sendMail(restartoption, function (error, info) {
+        mail.sendMail(conf['restartoption'], function (error, info) {
             console.log('mail_error: ' + error);
             var date = new Date();
             console.log(date + '湖北监控服务异常已恢复');
@@ -69,7 +39,7 @@ setInterval(function () {
     if (currentTime - lastNotifyTime > 5 * 60 * 1000) {
         //5分钟内，没收到消息，就认为监控工具死了，我要发邮件
         if (restart && isErrorSend) {
-            mail.sendMail(erroroption, function (error, info) {
+            mail.sendMail(conf['erroroption'], function (error, info) {
                 console.log('mail_error: ' + error);
                 var date = new Date();
                 console.log(date + '湖北监控服务异常，请及时修复！');
